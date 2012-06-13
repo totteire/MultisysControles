5d1e343c5e5e7b6cf58dfc71239b7d1345c389ce
totteire <totteire@yahoo.com>
2012-06-13 10:18:43 +0200

    mise en page faite pour CDV :) plus qu'à coder le php

--- ConstatDeVerification.php
+++ ConstatDeVerification.php
@@ -14,10 +14,18 @@
     .footer{
         width:100%;
         position: absolute;
-        bottom: 35px;
+        bottom: 10px;
+        height: 5px;
     }
     .container{
         margin: 0 10px 0 100px;
+        height: 100%
+    }
+    table.spaced td{
+        padding: 5px 5px;
+    }
+    table.MM td{
+        padding: 2px 5px;
     }
     .titre{
         margin: auto;
@@ -32,16 +40,21 @@
         width: 400px;
         text-align:center;
     }
+    .fauxFooter{
+        position: absolute;
+        bottom: 70px;
+    }
+    
 </style>
 <page orientation='portrait' format='A4' style='' backimg='./img/PAPIERENTETE.jpg'>
     <div class='container' style=''>
-        <div class='titre' style=''><h1>Constat de Vérification</h1></div>
+        <div class='titre' style=''><h1>CONSTAT DE VÉRIFICATION</h1></div>
         <div class='numero' style=''><h1>N° 8029</h1></div>
 
         <table class='normale' cellspacing='0' style='text-align: left;'>
             <tr>
                 <td>Délivré à: </td>
-                <td style='padding-left:15px;'>M. Albert Dupont</td>
+                <td style='padding-left:15px;'><b>M. Albert Dupont</b></td>
             </tr>
             <tr>
                 <td></td>
@@ -56,36 +69,36 @@
         <table cellspacing='0' style='width: 100%;'>
         <tr>
             <td style='width:40%'>
-                <table class='normale' cellspacing='0' style='text-align: left; float: left;display: inline;'>
-                    <tr style='padding-top: 10px;'>
-                        <td>Désignation:</td>
-                        <td style='padding-left:15px;'><b>ALIMENTATION</b></td>
+                <table class='normale spaced' cellspacing='0' style='text-align: left; float: left;display: inline;'>
+                    <tr style=''>
+                        <td style=''>Désignation:</td>
+                        <td style=''><b>ALIMENTATION</b></td>
                     </tr>
-                    <tr style='padding-top: 10px;'>
-                        <td>Constructeur:</td>
-                        <td style='padding-left:15px;'><b>METRIX</b></td>
+                    <tr style=>
+                        <td style=''>Constructeur:</td>
+                        <td style=''><b>METRIX</b></td>
                     </tr>
                 </table>
             </td><td style='width:40%'>
-                <table cellspacing='0' style='display: inline;text-align: left; font-size: 11pt;'>
-                    <tr style='padding-top: 10px;'>
+                <table class='normale spaced' cellspacing='0' style='display: inline;text-align: left;'>
+                    <tr style=''>
                         <td>Type:</td>
-                        <td style='padding-left:15px;'><b>ALIMENTATION</b></td>
+                        <td style=''><b>ALIMENTATION</b></td>
                     </tr>
-                    <tr style='padding-top: 10px;'>
+                    <tr style=''>
                         <td>N° de série:</td>
-                        <td style='padding-left:15px;'><b>AX322</b></td>
+                        <td style=''><b>AX322</b></td>
                     </tr>
-                    <tr style='padding-top: 10px;'>
+                    <tr style=''>
                         <td>N° de Chassis:</td>
-                        <td style='padding-left:15px;'><b>N°3</b></td>
+                        <td style=''><b>N°3</b></td>
                     </tr>
                 </table>
             </td>
         </tr>
         </table>
-        <h2>¤ MOYEN DE VÉRIFICATION UTILISÉ ¤</h2>
-        <table class='normale' cellspacing='0' style='width: 100%;'>
+        <h2>¤ MOYENS DE VÉRIFICATION UTILISÉS ¤</h2>
+        <table class='normale MM' cellspacing='0' style='width: 100%;'>
             <tr>
                 <td>Calibrateur FLUKE 5500A N° de série 8675008</td><td></td><td>COFRAC N° E12-057 du 02/12</td>
             </tr>
@@ -107,7 +120,7 @@
             </tr>
         </table>
         <h2>¤ TECHNICIEN RESPONSABLE DE LA VÉRIFICATION ¤</h2>
-        <table class'normale' cellspacing='0' style=''>
+        <table class='normale spaced' cellspacing='0' style=''>
             <tr>
                 <td>Nom :</td>
                 <td><b>C.FONTES</b></td>
@@ -119,15 +132,17 @@
             <tr>
                 <td>Ce constat comprend :</td>
                 <td><b>2 pages</b></td>
-                <td><b>Date de vérification : 21/05/2012</b></td>
+                <td style='padding-left:40px;'><b>Date de vérification : 21/05/2012</b></td>
             </tr>
         </table>
-        <div class='small' style='width:300px;margin:auto;'>
-            La reproduction de ce document n'est autorisée que sous forme de fac-similé photographique intégral.
-        </div>
-        <div class='small'>
-            1-Ce document ne peut être utilisé en lieu et place d'un certificat d'étalonnage.<br>
-            2-Ce document est réalisé conformément à la norme NF X07-011 définissant les constats de vérification et selon l'instruction interne MULTISYS.
+        <div class='fauxFooter'>
+            <div class='small' style='width:350px;margin:auto;text-align:center;'>
+                La reproduction de ce document n'est autorisée que sous forme de fac-similé photographique intégral.
+            </div>
+            <div class='small' style='margin-top: 15px;'>
+                1-Ce document ne peut être utilisé en lieu et place d'un certificat d'étalonnage.<br>
+                2-Ce document est réalisé conformément à la norme NF X07-011 définissant les constats de vérification et selon l'instruction interne MULTISYS.
+            </div>
         </div>
         <table class='small footer' cellspacing='0' style=''>
             <tr>
@@ -138,74 +153,32 @@
         </table>
     </div>
 </page>
+
+
+
+
 <page orientation='portrait' format='A4' style='font-size: 12pt;' backimg='./img/PAPIERENTETE.jpg'>
     <div class='container' style=''>
-        <div class='titre' style=''><h1>Constat de Vérification</h1></div>
+        <div class='titre' style=''><h1>CONSTAT DE VÉRIFICATION</h1></div>
         <div class='numero' style=''><h1>N° 8029</h1></div>
-        <h2>¤ MÉTHODE DE MESURE EMPLOYÉE ¤</h2>
-        <p class='normale' style='margin-left:50px;'>Mesures effectuées par comparaison ou par injection avec les étalons de référence de l'entreprise raccordés COFRAC.</p>
+        <h2>¤ MÉTHODE DE MESURES EMPLOYÉE ¤</h2>
+        <p class='normale' style=''>Mesures effectuées par comparaison ou par injection avec les étalons de référence de l'entreprise raccordés COFRAC.</p>
         <h2>¤ CONDITIONS DE MESURES ¤</h2>
-        <p class='normale' style='margin-left:50px;'>Température: <b>23°+/-2°C</b></p>
+        <p class='normale' style=''>Température: <b>23°+/-2°C</b></p>
         <h2>¤ LISTE DES PARAMÈTRES VÉRIFIÉS ¤</h2>
-        <table class='normale' cellspacing='0' style='width:100%'>
+        <table class='normale spaced' cellspacing='0' style='width:100%'>
             <tr>
-                <td style=''>( )Amplitudes</td>
-                <td style=''>( )Isolement</td>
+                <td style=''>- Courants</td>
             </tr>
             <tr>
-                <td style=''>( )Bandes passantes</td>
-                <td style=''>( )Luminance</td>
-            </tr>
-            <tr>
-                <td style=''>( )Capacités</td>
-                <td style=''>( )Modulation</td>
-            </tr>
-            <tr>
-                <td style=''>( )Compensations</td>
-                <td style=''>( )Pression</td>
-            </tr>
-            <tr>
-                <td style=''>( )Comptage</td>
-                <td style=''>( )Puissances</td>
-            </tr>
-            <tr>
-                <td style=''>(&nbsp;&nbsp;)Continuité</td>
-                <td style=''>( )Résistances</td>
-            </tr>
-            <tr>
-                <td style=''>(X)Courants</td>
-                <td style=''>( )Résistivité</td>
-            </tr>
-            <tr>
-                <td style=''>( )Dimensionnel</td>
-                <td style=''>( )Rotation de phase</td>
-            </tr>
-            <tr>
-                <td style=''>( )Distortion</td>
-                <td style=''>( )Tachymétrie</td>
-            </tr>
-            <tr>
-                <td style=''>( )Eclairement</td>
-                <td style=''>( )Températures</td>
-            </tr>
-            <tr>
-                <td style=''>( )Facteur de puissance</td>
-                <td style=''>( )Temps/Période</td>
-            </tr>
-            <tr>
-                <td style=''>( )Fréquences</td>
-                <td style=''>(X)Tensions</td>
-            </tr>
-            <tr>
-                <td style=''>( )Inductances</td>
-                <td style=''>( )Thermocouples</td>
+                <td style=''>- Tensions</td>
             </tr>
         </table>
         <h2>¤ JUGEMENT ¤</h2>
         <table class='normale' cellspacing='0' style=''>
             <tr><td>(X) Appareil conforme aux spécifications énoncées par le constructeur pour les gammes vérifiées.</td></tr>
-            <tr><td>( ) Appareil conforme aux spécifications énoncées par le constructeur après réparation.</td></tr>
-            <tr><td>( ) Appareil non conforme.</td></tr>
+            <tr><td>(&nbsp;&nbsp;) Appareil conforme aux spécifications énoncées par le constructeur après réparation.</td></tr>
+            <tr><td>(&nbsp;&nbsp;) Appareil non conforme.</td></tr>
         </table>
         <h2>¤ OBSERVATION ¤</h2>
         <p class='normale'>NÉANT</p>
