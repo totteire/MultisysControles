beea0fd700a3c3da9b423c13c0bb3a920abe30c9
totteire <totteire@yahoo.com>
2012-07-31 11:41:40 +0200

    ajout numC dans controle.php

--- controle.php
+++ controle.php
@@ -1,9 +1,9 @@
 <table id="tableau" class="tableau tablesorter">
-	<thead><tr><th class='info'></th><th class="topLeft">Numéro</th><th>Type</th><th>Date</th><th>Client</th><th>APP-Designation</th><th>APP-Marque</th><th>APP-Type</th><th>Num Série</th><th class="topRight">Action</th></tr></thead>
+	<thead><tr><th class='info'></th><th class="topLeft">Numéro</th><th>Type</th><th>Date</th><th>Client</th><th>APP-Designation</th><th>APP-Marque</th><th>APP-Type</th><th>Num Série</th><th>Num Chassis</th><th class="topRight">Action</th></tr></thead>
     <tbody>
 	<?php
 		include('connect.php');
-		$req="SELECT CONTROLE.ID,CONTROLE.NUM,CONTROLE.TYPE_CTRL,CONTROLE.DATE,CONTROLE.NUM_SERIE, NOM, DESIGNATION,MARQUE,TYPE,TECHNICIEN FROM CONTROLE,CLIENT C, APP_DESI, APP_MARQUE, APP_TYPE WHERE C.ID=CONTROLE.ID_AVOIR AND APP_DESI.ID=CONTROLE.ID_APP_DESI AND APP_MARQUE.ID=CONTROLE.ID_APP_MARQUE AND APP_TYPE.ID=CONTROLE.ID_APP_TYPE ORDER BY CONTROLE.ID DESC;";
+		$req="SELECT CONTROLE.ID,CONTROLE.NUM,CONTROLE.TYPE_CTRL,CONTROLE.DATE,CONTROLE.NUM_SERIE,CONTROLE.NUM_CHASSIS, NOM, DESIGNATION,MARQUE,TYPE,TECHNICIEN FROM CONTROLE,CLIENT C, APP_DESI, APP_MARQUE, APP_TYPE WHERE C.ID=CONTROLE.ID_AVOIR AND APP_DESI.ID=CONTROLE.ID_APP_DESI AND APP_MARQUE.ID=CONTROLE.ID_APP_MARQUE AND APP_TYPE.ID=CONTROLE.ID_APP_TYPE ORDER BY CONTROLE.ID DESC;";
 		$nb=0;
 		$result=mysql_query($req) or die(mysql_error());
 		while($res=mysql_fetch_array($result))
@@ -21,6 +21,7 @@
 			    <td id='TDappD'>".$res['MARQUE']."</td>
 			    <td id='TDappT'>".$res['TYPE']."</td>
 			    <td id='TDnumS'>".$res['NUM_SERIE']."</td>
+			    <td id='TDnumC'>".$res['NUM_CHASSIS']."</td>
 			    <td id='modif' class='modifCell'><img class='modifCtrl' src='img/modify.png'/><img class='suppr' src='img/delete.png'/><a href='generatePdf.php?id=".$res['ID']."' onclick='' class='pdfEdit'><img src='img/pdf.png'/></a></td>
 		    </tr>";
 		    $nb++;
