07224336a7004b07d2b4f4f25ba4c3493f5c919a
tibo or Manu in Multisys <totteire@yahoo.com>
2012-09-21 09:50:02 +0200

    modif ajoutMes.php, added field ordre in SQL

--- FicheIntervention.php
+++ FicheIntervention.php
@@ -1,4 +1,4 @@
-<style type='text/css'>
+﻿<style type='text/css'>
 
     .normale{
         font-size: 11pt;
@@ -73,7 +73,7 @@
         <div class='' style='width:100%;margin:0 auto;text-align:center;font-size:15px;font-style:italic;'>Intervention Sheet</div>
         <div class='numero' style=''><h1>N°<?php echo " ".$res['NUM'];?></h1></div>
 
-	<div class='' style="margin-top: 50px;">Toulouse, le <?php setlocale(LC_TIME, 'fr_FR.utf8','fra');$date = $res['DATE'];echo strftime("%d %B %Y",strtotime("$date"));?></div>
+	<div class='' style="margin-top: 50px;">Toulouse, le <?php setlocale(LC_TIME, 'french');$date = $res['DATE'];echo strftime("%d %B %Y",strtotime("$date"));?></div>
         <table class='normale' cellspacing='0' style='text-align: left; margin-top:50px;'>
             <tr>
                 <td>Délivré à: </td>
--- moyenMesure/ajoutMes.php
+++ moyenMesure/ajoutMes.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 $id=trim($_POST['id']);
 $libelle=trim($_POST['libelle']);
@@ -9,7 +9,11 @@ if ($libelle && $nomVerif && $numDateVerif){
     include("../connect.php");
     $test = mysql_query("SELECT * FROM MOYEN_MESURE WHERE LIBELLE='$libelle' AND NOM_VERIF='$nomVerif' AND NUM_DATE_VERIF='$numDateVerif';")or die(mysql_error());
     if (mysql_num_rows($test)==0){
-        $req = mysql_query("INSERT INTO MOYEN_MESURE VALUES (NULL,'$libelle','$nomVerif','$numDateVerif');") or die(mysql_error());
+	if(strpos($nomVerif,'COFRAC')) $ordre = 1;
+	else if(strpos($nomVerif, 'CONSTAT'))$ordre = 2;
+	else if(strpos($nomVerif, 'MULTISYS'))$ordre = 3;
+	else $ordre = 0;
+        $req = mysql_query("INSERT INTO MOYEN_MESURE VALUES (NULL,'$libelle','$nomVerif','$numDateVerif', '$ordre');") or die(mysql_error());
     	$return['error'] = false;
 	    $return['msg'] = "Le moyen de mesure: ".$libelle." a bien été enregistré!";
 	}else{
